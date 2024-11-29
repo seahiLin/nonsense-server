f787b69174dd12001dfe67c11e7487af51c50dd5
seahi <2296809118@qq.com>
2024-11-29 10:59:51 +0800

    feat: change port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
@@ -13,14 +14,18 @@ import (
 type AgentServer struct{}
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	server := &AgentServer{}
 	mux := http.NewServeMux()
 	path, handler := servicesconnect.NewAgentServiceHandler(server)
 	mux.Handle(path, handler)
 
-	log.Println("Starting server on :8080")
+	log.Println("Starting server on :" + port)
 	if err := http.ListenAndServe(
-		"localhost:8080",
+		"0.0.0.0:"+port,
 		h2c.NewHandler(mux, &http2.Server{}),
 	); err != nil {
 		log.Fatal(err)
